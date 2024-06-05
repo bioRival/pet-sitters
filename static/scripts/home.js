dcceddfc987d8535cf3ab7938323e70639be4de7
BioRival <biorivalmain@gmail.com>
2024-06-05 21:17:17 +0300

    добавлена секция services v1.0

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -9,18 +9,15 @@ function bannerSlideshow() {
     let current = 0 // index of image visible
 
     function slideshow() {
-        // images.classList.remove('banner__show-image')
-        // document.querySelectorAll('.banner__image').classList.remove('banner__show-image')
         for (let i = 0; i < images.length; i++) {
-            images[i].style.opacity = 0
-            images[i].style.transform = `translateX(1rem)`
+            images[i].classList.remove('banner__show-image')
         }
+
         current = (current != images.length - 1) ? current + 1 : 0
 
-        images[current].style.opacity = 1
-        images[current].style.transform = `translateX(-1rem)`
-        // images[current].classList.add('banner__show-image')
+        images[current].classList.add('banner__show-image')
     }
+    
     slideshow(); // Execute on loading the page first
     setInterval(slideshow, 5000); // Setting the time in ms
 }
