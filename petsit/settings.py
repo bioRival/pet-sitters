fe3b0758eda05798f7e0da5d3ddb4c4e7617d23e
BioRival <biorivalmain@gmail.com>
2024-06-20 18:54:54 +0300

    vercel test

--- petsit/settings.py
+++ petsit/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 # Application definition
 
@@ -96,7 +96,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'petsit.wsgi.application'
+WSGI_APPLICATION = 'petsit.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
